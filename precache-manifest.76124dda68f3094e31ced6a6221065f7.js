3097a424a739c53c5898c76c693b640b0da6c60b
Rahim Ansari <rahimansari4560@gmail.com>
2022-07-31 11:55:14 +0530

    Updates

---
+++ precache-manifest.76124dda68f3094e31ced6a6221065f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad2b515ca1461aea8472a9751358d30",
+    "url": "/reduxapp/index.html"
+  },
+  {
+    "revision": "fe5481988ae7d6f6bdfc",
+    "url": "/reduxapp/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "06dc3de83c211fea28f6",
+    "url": "/reduxapp/static/js/2.c6c9f59d.chunk.js"
+  },
+  {
+    "revision": "fe5481988ae7d6f6bdfc",
+    "url": "/reduxapp/static/js/main.11fca4f2.chunk.js"
+  },
+  {
+    "revision": "05f2bc6ac9b59172b51c",
+    "url": "/reduxapp/static/js/runtime~main.13d3e369.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reduxapp/static/media/logo.5d5d9eef.svg"
+  }
+]);
